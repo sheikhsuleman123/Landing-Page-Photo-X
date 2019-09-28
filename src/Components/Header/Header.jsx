e197bbff174cea06d01a49d950beb7cc4d43d24e
sheikhsuleman123 <mssheikh333@gmail.com>
2019-09-28 19:08:39 +0500

    first commit banner is designing now

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './header.css';
+export default class componentName extends Component {
+  render() {
+    return (
+    
+      <header>
+
+   <div className="text-light text-right">
+    <h1 className="display-4">
+        Welcome to <span >PHOTO </span>  <span className="display-3"> X</span>
+    </h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+   </div>
+      </header>
+    );
+  }
+}
