4ea54453ea71855a4d1b736d0db5263ce03041d2
sheikhsuleman123 <mssheikh333@gmail.com>
2019-10-14 21:31:27 +0500

    footer section is completed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -57,7 +57,7 @@ export default class Footer extends Component {
                 <div className="col-lg-4 col-sm-6">
                     <h5 className="pb-3">Stay Connected</h5>
                     <p className="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, vel rerum, repudiandae quam incidunt nihil deleniti debitis unde pariatur non ullam consequatur nisi magnam, nesciunt dolore odio perferendis rem assumenda?</p>
-                <form action="">
+                <form className="mb-3">
                     <div className="input-group">
                         <input type="text" className="form-control" placeholder="Email Address"/>
                         <div className="input-group-append">
@@ -67,9 +67,18 @@ export default class Footer extends Component {
                         </div>
                     </div>
                 </form>
-                <ul className="list-inline">
-                    
+                <ul className="list-inline py-3">
+                    <li className="list-inline-item"><i className="fab fa-facebook-square fa-2x text-primary"></i> </li>
+                    <li className="list-inline-item"><i className="fab fa-google-plus fa-2x text-danger"></i> </li>
+                    <li className="list-inline-item"><i className="fab fa-instagram fa-2x text-danger"></i> </li>
+                    <li className="list-inline-item"><i className="fab fa-twitter fa-2x text-info"></i> </li>
+                    <li className="list-inline-item"><i className="fab fa-youtube fa-2x text-danger"></i> </li>
                 </ul>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col text-center text-light border-top pt-3">
+                    <p>&copy; 2019 Copyright All Right Reserved</p>
                 </div>
             </div>
         </div>
